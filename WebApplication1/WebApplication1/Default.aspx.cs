885573f8f912fd46584af57ef06db2c747746ee4
Łukasz Seremak <33153470+LSeremak@users.noreply.github.com>
2019-03-26 13:34:57 +0100

    Test

---
+++ WebApplication1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Btn1.Text = "Proszę kliknij mnie";
+            }
+            else
+            {
+                Btn1.Text = TbName.Text;
+                }
+            
+        }
+
+        protected void Btn1_Click(object sender, EventArgs e)
+        {
+            Btn1.Text = TbName.Text;
+        }
+    }
+}
